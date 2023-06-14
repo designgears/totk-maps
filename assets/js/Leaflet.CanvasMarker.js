42eead89d83ecbbe7b0b256299b195bca618f994
designgears <bobby@designgears.com>
2023-06-14 00:45:08 -0600

    actually remove circle drawing

--- assets/js/Leaflet.CanvasMarker.js
+++ assets/js/Leaflet.CanvasMarker.js
@@ -1,10 +1,7 @@
 L.CanvasMarker = L.Path.extend({
     options: {
-        stroke: false,
-        iconAnchor: false,
         icon: false,
-        color: false,
-        interactive: true
+        iconAnchor: false
     },
 
     initialize: function (latlng, options) {
@@ -13,19 +10,17 @@ L.CanvasMarker = L.Path.extend({
         var layer = this;
         this._latlng = L.latLng(latlng);
 
-        if (this.options.icon) {
-            this._icon = new Image();
-            this._icon.onload = function() {
-                if (!layer.options.iconAnchor) {
-                    layer._icon.anchorWidth = Math.round(this.width / 2);
-                    layer._icon.anchorHeight = Math.round(this.height / 2);
-                } else {
-                    layer._icon.anchorWidth = Math.round(layer.options.iconAnchor[0]);
-                    layer._icon.anchorHeight = Math.round(layer.options.iconAnchor[1]);
-                }
+        this._icon = new Image();
+        this._icon.onload = function() {
+            if (!layer.options.iconAnchor) {
+                layer._icon.anchorWidth = Math.round(this.width / 2);
+                layer._icon.anchorHeight = Math.round(this.height / 2);
+            } else {
+                layer._icon.anchorWidth = Math.round(layer.options.iconAnchor[0]);
+                layer._icon.anchorHeight = Math.round(layer.options.iconAnchor[1]);
             }
-            this._icon.src = this.options.icon;
         }
+        this._icon.src = this.options.icon;
 
         this._check = new Image();
         this._check.src = '/assets/img/check.png';
@@ -113,13 +108,9 @@ L.CanvasMarker = L.Path.extend({
     },
 
     _updateBounds: function () {
-        if (this.options.icon) {
-            var width = this._icon.width,
+        var width = this._icon.width,
             height = this._icon.height;
-        } else {
-            var width = 12,
-            height = 12;
-        }
+
         var w = 50,
             topleft = [width + w, height + w],
             bottomright = [width + w, w];
@@ -138,33 +129,17 @@ L.CanvasMarker = L.Path.extend({
     },
 
     _empty: function () {
-        if (this.options.icon) {
-            return this._icon.src && !this._renderer._bounds.intersects(this._pxBounds);
-        } else {
-            return !this._renderer._bounds.intersects(this._pxBounds);
-        }
+        return this._icon.src && !this._renderer._bounds.intersects(this._pxBounds);
     },
 
     _containsPoint: function (p) {
-        if (this.options.icon) {
-            var tX = Math.round(this._icon.width / 2),
-                tY = Math.round(this._icon.height / 2);
-            
-            return (p.x - (tX - this._icon.anchorWidth) <= this._point.x + tX) &&
-                (p.x -(tX - this._icon.anchorWidth) >= this._point.x - tX) &&
-                (p.y -(tY - this._icon.anchorHeight) <= this._point.y + tY) &&
-                (p.y -(tY - this._icon.anchorHeight) >= this._point.y - tY);
-        } else {
-            var tX = 8,
-                tY = 8,
-                anchorWidth = 8,
-                anchorHeight = 8;
-
-            return (p.x - (tX - anchorWidth) <= this._point.x + tX) &&
-                (p.x - (tX - anchorWidth) >= this._point.x - tX) &&
-                (p.y - (tY - anchorHeight) <= this._point.y + tY) &&
-                (p.y - (tY - anchorHeight) >= this._point.y - tY);
-        }
+        var tX = Math.round(this._icon.width / 2),
+            tY = Math.round(this._icon.height / 2);
+        
+        return (p.x - (tX - this._icon.anchorWidth) <= this._point.x + tX) &&
+            (p.x -(tX - this._icon.anchorWidth) >= this._point.x - tX) &&
+            (p.y -(tY - this._icon.anchorHeight) <= this._point.y + tY) &&
+            (p.y -(tY - this._icon.anchorHeight) >= this._point.y - tY);
     },
 
 });
@@ -177,23 +152,11 @@ L.Canvas.include({
     _updateCanvasMarker: function (layer) {
         if (layer._empty()) { return; }
         var p = layer._point,
-            ctx = this._ctx,
-            radius = 5;
+            ctx = this._ctx;
 
         ctx.save();
 
-        if (layer.options.icon) {
-            ctx.drawImage(layer._icon, p.x - layer._icon.anchorWidth, p.y - layer._icon.anchorHeight);
-        } else {
-            ctx.beginPath();
-            ctx.arc(p.x, p.y, radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = layer.options.color;
-            ctx.fill();
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = '#000000';
-            ctx.stroke();
-        }
-
+        ctx.drawImage(layer._icon, p.x - layer._icon.anchorWidth, p.y - layer._icon.anchorHeight);
         if (layer.feature.properties.completed) {
             ctx.drawImage(layer._check, p.x - 7, p.y - 7);
         }
